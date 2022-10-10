aba1d27fb0694146b3461b114874f5a5639cdc1b
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2022-10-10 11:38:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ parser = argparse.ArgumentParser()
 # ========  Experiments Name ================
 parser.add_argument('--save_dir',               default='experiments_logs',         type=str, help='Directory containing all experiments')
 parser.add_argument('--experiment_description', default='test',   type=str, help='experiment name')
-parser.add_argument('--run_description',        default='attn',     type=str, help='run name')
+parser.add_argument('--run_description',        default='run_description',     type=str, help='run name')
 
 # ========= Select methods ============
-parser.add_argument('--sleep_model',              default='resnet1d',          type=str, help='dsn, attnsleep, cnn1d')
+parser.add_argument('--sleep_model',              default='dsn',          type=str, help='dsn, attnsleep, cnn1d')
 parser.add_argument('--ssl_method',              default='simclr',        type=str, help='simclr, cpc, ts_tcc, clsTran')
-parser.add_argument('--train_mode',              default='supervised',            type=str, help='supervised, ssl, (ft)fine_tune, (lc)linear_classifier')
+parser.add_argument('--train_mode',              default='ssl',            type=str, help='supervised, ssl, (ft)fine_tune, (lc)linear_classifier')
 parser.add_argument('--oversample',              default=False,           type=bool, help='apply oversampling or not?')
 
 # ========= Select the DATASET ==============
@@ -28,7 +28,7 @@ parser.add_argument('--augmentation',           default='noise_permute',   type=
 # ========= Experiment settings ===============
 parser.add_argument('--data_path',              default=r'data',           type=str,   help='Path containing dataset')
 parser.add_argument('--num_runs',               default=1,                 type=int,   help='Number of consecutive run with different seeds')
-parser.add_argument('--device',                 default='cuda:1',            type=str,   help='cpu or cuda')
+parser.add_argument('--device',                 default='cuda:0',            type=str,   help='cpu or cuda')
 
 
 args = parser.parse_args()
